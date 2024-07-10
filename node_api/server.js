79194f40d82cd7ce083b0175c360f6247e61331c
Shivam Menda <shivammenda2002@gmail.com>
2024-07-10 16:34:54 +0530

    Add prometheus logging

--- node_api/server.js
+++ node_api/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import {config} from 'dotenv';
 import morgan from 'morgan';
+import { collectDefaultMetrics,Registry } from 'prom-client';
 import authRouter from './routes/auth.js';
 import userRouter from './routes/user.js';
 import doctorRouter from './routes/doctor.js';
@@ -17,7 +17,11 @@ app.use(cors());
 config({path:'./config.env'});
 
 await connectDB();
-
+const register= new Registry()
+register.setDefaultLabels({
+    app: 'aetherwell-api'
+});
+collectDefaultMetrics({register:register});
 
 if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'testing') {
     app.use(morgan('dev'));
@@ -33,6 +37,15 @@ app.use("/api/v1/doctors",userAuth,rateLimiterMiddleware,doctorRouter);
 app.get('/',(req,res)=>{
     res.status(200).send('AetherWell api running');
 });
+
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', register.contentType);
+    register.metrics().then(metrics => {
+        res.status(200).send(metrics);
+    });
+}
+);
+
 app.get('/api/v1/',(req,res)=>{
     res.json({
         status: "success",
